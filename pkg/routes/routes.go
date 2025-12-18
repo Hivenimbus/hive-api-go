1cc3febb99408e572e2c0371a75281518b4d21a5
Hivenimbus <hivenimbus@gmail.com>
2025-12-18 19:59:30 -0300

    Reapply "Update routes.go"
    This reverts commit df7f401b4d92909dd341f5c647b5e69e3d891f3a.

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -87,6 +87,8 @@ func (r *Routes) AssignRoutes(eng *gin.Engine) {
 			routes.DELETE("/proxy/:instanceId", r.instanceHandler.DeleteProxy)
 			routes.POST("/forcereconnect/:instanceId", r.instanceHandler.ForceReconnect)
 			routes.GET("/logs/:instanceId", r.instanceHandler.GetLogs)
+			routes.GET("/:instanceId/advanced-settings", r.instanceHandler.GetAdvancedSettings)
+			routes.PUT("/:instanceId/advanced-settings", r.instanceHandler.UpdateAdvancedSettings)
 		}
 	}
 
@@ -101,8 +103,6 @@ func (r *Routes) AssignRoutes(eng *gin.Engine) {
 			routes.POST("/disconnect", r.instanceHandler.Disconnect)
 			routes.POST("/reconnect", r.instanceHandler.Reconnect)
 			routes.DELETE("/logout", r.instanceHandler.Logout)
-			routes.GET("/:instanceId/advanced-settings", r.instanceHandler.GetAdvancedSettings)
-			routes.PUT("/:instanceId/advanced-settings", r.instanceHandler.UpdateAdvancedSettings)
 		}
 	}
 
